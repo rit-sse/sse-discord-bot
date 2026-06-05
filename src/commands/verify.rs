e9974ecfe303d991b40a21d5fab60e9c9eb729f8
Cayden Tisdale <caydentisdale43@gmail.com>
2026-06-05 18:51:23 -0400

    feat: add verify command and move create dedicated commands folder

---
+++ src/commands/verify.rs
@@ -0,0 +1,9 @@
+use crate::{Context, Error};
+use poise::serenity_prelude as serenity;
+#[poise::command(slash_command)]
+pub async fn verify(ctx: Context<'_>, user: Option<serenity::User>) -> Result<(), Error> {
+    let user = user.as_ref().unwrap_or_else(|| ctx.author());
+    let res = format!("Verifying: {}", user.name);
+    ctx.say(res).await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
 use poise::serenity_prelude as serenity;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+use crate::commands::{age, verify};
+pub mod commands;
+pub struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-
-/// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
-async fn age(
-    ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
-) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
@@ -26,7 +15,7 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age()],
+            commands: vec![age::age(), verify::verify()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
