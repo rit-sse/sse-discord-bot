47838a5c89a1241c42adf897e72c920f5d8fd288
Cayden Tisdale <caydentisdale43@gmail.com>
2026-06-13 14:30:09 -0400

    chore: load dotenv before logger initialization

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,21 @@
 use tracing_subscriber::{EnvFilter, fmt};
 
+pub fn load_dotenv() -> anyhow::Result<()> {
+    match dotenvy::dotenv() {
+        Ok(_) => Ok(()),
+        Err(err) if err.not_found() => Ok(()),
+        Err(err) => Err(err.into()),
+    }
+}
+
 pub fn init() {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        if cfg!(debug_assertions) {
+            EnvFilter::new("debug")
+        } else {
+            EnvFilter::new("info")
+        }
+    });
 
     fmt().with_env_filter(env_filter).init()
 }
